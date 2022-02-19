785f5c48bbc6f068260527eea60fc304ddcacd1b
Shamil Imandinov <flip0173@gmail.com>
2022-02-19 19:37:07 +0400

    fix

--- src/components/Competitions/MyPagination.js
+++ src/components/Competitions/MyPagination.js
@@ -1,46 +1,117 @@
-import React,{useEffect, useState} from 'react';
+import React from 'react';
 import Pagination from 'react-bootstrap/Pagination'
 
 const MyPagination = ({competitionsPerPage,totalCompetitions,currentPage,paginate})=> {
-  let active = currentPage;
+  
   const totalPages=Math.ceil(totalCompetitions/competitionsPerPage)
-  console.log(totalPages)
-  console.log(currentPage)
-  const firstPage=1;
-  let items = [];
-  const ellips=<Pagination.Ellipsis/>;
- 
- 
-    for(let i=firstPage;i<=totalPages;i++){
-      if((i===firstPage) || (i===currentPage-1)|| (i<currentPage+4)||(i===totalPages)){
-        items.push(
-        <Pagination.Item key={i} active={i === active}  onClick={()=>paginate(i)}>
-            {i}
-        </Pagination.Item>)
-      } 
-      else{
-        i=totalPages-1
-        items.push(<Pagination.Ellipsis/>)
+  let isPageNumberOutOfRange;
+
+  const pageNumbers = [...new Array(totalPages)].map((_, index) => {
+    const pageNumber = index+1;
+    
+    if(totalPages>7){
+      //Номер текущей страницы
+      const isPageNumberFirst = pageNumber === 1;
+      const isPageNumberLast = pageNumber === totalPages;
+      const isCurrentPageFirst=currentPage===1;
+      const isCurrentPageLast=currentPage===totalPages;
+      const isCurrentPageSecond=(currentPage===2);
+      const isCurrentPagePreLast=(currentPage===totalPages-1);
+      const isCurrentPageBetweenLeft=((currentPage>1)&&(currentPage<4));
+      const isCurrentPageBetweenRight=((currentPage>totalPages-3)&&(currentPage<=totalPages));
+      const isCurrentPageMiddle=((currentPage>=4)&&(currentPage<=totalPages-3));
+
+
+      //Отображение номеров страниц в зависимости от положения текущей страницы
+      const isCurrentPageFirstLast = Math.abs(pageNumber - currentPage) <= 4;
+      const isCurrentPageBetween = Math.abs(pageNumber - currentPage) <= 2;
+      const isCurrentPageSecondPreLast = Math.abs(pageNumber - currentPage) <= 3;
+      const isCurrentPageMiddlePos = Math.abs(pageNumber - currentPage) <= 1;
+    
+      if (
+      ((isCurrentPageFirst || isCurrentPageLast) && isCurrentPageFirstLast) || (isPageNumberFirst||isPageNumberLast)
+      ) {
+        isPageNumberOutOfRange = false;
+        return (
+          <Pagination.Item
+            key={pageNumber}
+            onClick={() => paginate(pageNumber)}
+            active={pageNumber === currentPage}
+          >
+            {pageNumber}
+          </Pagination.Item>
+        );
       }
-      if((i<currentPage) &&  (i>firstPage+1)) {
-        items.splice(firstPage,2,<Pagination.Ellipsis/>)
+      if (
+        (isCurrentPageSecond||isCurrentPagePreLast) && isCurrentPageSecondPreLast
+        ) {
+          isPageNumberOutOfRange = false;
+          return (
+            <Pagination.Item
+              key={pageNumber}
+              onClick={() => paginate(pageNumber)}
+              active={pageNumber === currentPage}
+            >
+              {pageNumber}
+            </Pagination.Item>
+          );
+        }
+      if (
+      (isCurrentPageBetweenLeft||isCurrentPageBetweenRight) && isCurrentPageBetween
+      ) {
+        isPageNumberOutOfRange = false;
+        return (
+          <Pagination.Item
+            key={pageNumber}
+            onClick={() => paginate(pageNumber)}
+            active={pageNumber === currentPage}
+          >
+            {pageNumber}
+          </Pagination.Item>
+        );
       }
-      
+      if (
+        isCurrentPageMiddle && isCurrentPageMiddlePos
+      ) {
+        isPageNumberOutOfRange = false;
+        return (
+          <Pagination.Item
+            key={pageNumber}
+            onClick={() => paginate(pageNumber)}
+            active={pageNumber === currentPage}
+          >
+            {pageNumber}
+          </Pagination.Item>
+        );
+      }
+
+      if (!isPageNumberOutOfRange) {
+        isPageNumberOutOfRange = true;
+        return <Pagination.Ellipsis key={pageNumber} className="muted" />;
+      }
+    }else{
+      return (
+        <Pagination.Item
+          key={pageNumber}
+          onClick={() => paginate(pageNumber)}
+          active={pageNumber === currentPage}
+        >
+          {pageNumber}
+        </Pagination.Item>
+      );
     }
-
     
-   
-
-    return(
-    <Pagination className="pagination justify-content-center"> 
-        <Pagination.Prev  onClick={()=>paginate(pageNumber => pageNumber === 1 ? 1 : pageNumber - 1)}/>
-        {items}
-        <Pagination.Next  onClick={()=>paginate(pageNumber =>
-          pageNumber === Math.ceil(totalCompetitions / competitionsPerPage) ?
-              pageNumber : pageNumber + 1)}  />
-    </Pagination>
-    
-    
-    )
+    return null;
+  });
+  
+  return(
+  <Pagination className="pagination justify-content-center"> 
+      {totalPages>0?<Pagination.Prev  onClick={()=>paginate(pageNumber => pageNumber === 1 ? 1 : pageNumber - 1)}/>:null}
+      {pageNumbers}
+      {totalPages>0?<Pagination.Next  onClick={()=>paginate(pageNumber =>
+    pageNumber === Math.ceil(totalCompetitions / competitionsPerPage) ?
+        pageNumber : pageNumber + 1)}/>:null}
+  </Pagination>
+  )
 }
 export default MyPagination;
