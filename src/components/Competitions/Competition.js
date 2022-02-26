cbbcb8462e9a7a0356d79313b88cd6ae327f1d84
Shamil Imandinov <flip0173@gmail.com>
2022-02-26 16:08:13 +0400

    fix

--- src/components/Competitions/Competition.js
+++ src/components/Competitions/Competition.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import CompetitionItem from './CompetitionItem';
-import CardGroup from 'react-bootstrap/CardGroup'
+import React from 'react';
+import CardGroup from 'react-bootstrap/CardGroup';
+import {Link} from "react-router-dom";
+import Card from 'react-bootstrap/Card';
 
 
  function Competition({competitions, firstCompetitionPage,lastCompetitionPage,competitionsPerPage}) {
@@ -8,10 +9,16 @@ import CardGroup from 'react-bootstrap/CardGroup'
   const currentCompetitionPage= (competitions.slice(firstCompetitionPage,lastCompetitionPage)).length===0? competitions.slice(0,competitionsPerPage):competitions.slice(firstCompetitionPage,lastCompetitionPage)
   return (
     <CardGroup style={{ display: 'flex' }}>
-      
           {
             currentCompetitionPage.map((competition,i)=>(
-              <CompetitionItem competition={competition} key={i}/>
+              <Card border="dark" style={{ width: '20px' }}> 
+                <Link key={i} to={`/competitions/${competition.id}`}>
+                  <Card.Body class="card" key={i}>
+                    <Card.Text>{competition.name}</Card.Text>
+                    <Card.Text>{competition.area.name}</Card.Text>
+                  </Card.Body> 
+                </Link>
+              </Card>
             ))
           }
     </CardGroup>
